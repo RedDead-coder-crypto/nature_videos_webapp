e774be49a2520465d0cd42d39e07ae5e712848c9
RedDead-coder-crypto <mppool@gmx.de>
2025-06-06 20:53:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,22 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes import main as main_blueprint
+from models import db
 
 app = Flask(__name__)
-
-# ### 1.1 Datenbank‐Konfiguration ###
-# Lokales SQLite, in Render später evtl. eine Umgebungsvariable verwenden:
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# SQLAlchemy an Flask binden
+db.init_app(app)
 
-# ### 1.2 Blueprint registrieren ###
+# Blueprint registrieren
 app.register_blueprint(main_blueprint)
 
-# ### 1.3 Tabellen erstellen (falls nicht vorhanden) ###
+# Tabelle anlegen, falls sie noch nicht existiert
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    # Nur beim lokalen Testen:
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
