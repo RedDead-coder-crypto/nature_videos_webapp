3e955a11801a0d30a12bdf7ef8e41a35385e07a1
RedDead-coder-crypto <mppool@gmx.de>
2025-06-06 16:07:55 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,29 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import json
-
-# Instanz von SQLAlchemy
-db = SQLAlchemy()
 
 class Pipeline(db.Model):
-    __tablename__ = 'pipelines'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    type = db.Column(db.String(64), nullable=False)
-    schedule = db.Column(db.String(64), nullable=False)  # Cron-Expression
-    active = db.Column(db.Boolean, default=True)
-    settings = db.Column(db.Text, nullable=True)         # JSON-String für weitere Optionen
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    last_run = db.Column(db.DateTime, nullable=True)
-
-    def get_settings(self):
-        if self.settings:
-            return json.loads(self.settings)
-        return {}
-
-    def set_settings(self, settings_dict):
-        self.settings = json.dumps(settings_dict)
-
-    def __repr__(self):
-        return f"<Pipeline {self.id} - {self.name} ({self.type})>"
+    ...
+    status_text = db.Column(db.String(200), default="Wartet…")
+    started_at = db.Column(db.DateTime, default=None)
