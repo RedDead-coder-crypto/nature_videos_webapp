659a7f2e767f151f4fca7d16d6bd9e5514116b69
RedDead-coder-crypto <mppool@gmx.de>
2025-06-06 12:20:37 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
+
+# Instanz von SQLAlchemy
+db = SQLAlchemy()
+
+class Pipeline(db.Model):
+    __tablename__ = 'pipelines'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    type = db.Column(db.String(64), nullable=False)    
+    schedule = db.Column(db.String(64), nullable=False)  # Cron-Expression, z.B. '0 2 * * *'
+    active = db.Column(db.Boolean, default=True)
+    settings = db.Column(db.Text, nullable=True)         # JSON-String für weitere Optionen
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    last_run = db.Column(db.DateTime, nullable=True)
+
+    def get_settings(self):
+        if self.settings:
+            return json.loads(self.settings)
+        return {}
+
+    def set_settings(self, settings_dict):
+        self.settings = json.dumps(settings_dict)
+
+    def __repr__(self):
+        return f"<Pipeline {self.id} - {self.name} ({self.type})>"
