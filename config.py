235a987a03db5f870b10b8bd2e85c10b11503556
RedDead-coder-crypto <mppool@gmx.de>
2025-06-06 13:30:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,22 @@
 import os
 from dotenv import load_dotenv
 
-# Lade alle Umgebungsvariablen aus .env
 load_dotenv()
 
 class Config:
-    # Flask-Konfiguration
-    SECRET_KEY = os.environ.get("SECRET_KEY", "change_this")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "change_me")
     FLASK_ENV = os.environ.get("FLASK_ENV", "development")
 
-    # SQLAlchemy (hier SQLite)
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///nature_videos.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # YouTube API
     YOUTUBE_CLIENT_SECRETS_FILE = os.environ.get("YOUTUBE_CLIENT_SECRETS_FILE", "client_secrets.json")
     YOUTUBE_TOKEN_PICKLE = os.environ.get("YOUTUBE_TOKEN_PICKLE", "token_youtube_upload.pickle")
 
-    # Mediathek-Ordner
     AUDIO_DIR = os.environ.get("AUDIO_DIR", "media/audio")
     VIDEO_DIR = os.environ.get("VIDEO_DIR", "media/video")
     VIDEO_OUTPUT_DIR = os.environ.get("VIDEO_OUTPUT_DIR", "output_videos")
 
-    # Video-Metadaten (Templates)
     TITLE_TEMPLATE = os.environ.get(
         "TITLE_TEMPLATE",
         "Naturgeräusche – {length_min} Minuten Entspannung – Kein Text"
@@ -36,5 +30,4 @@ class Config:
         "Naturgeräusche,Entspannung,Ambient,ASMR"
     ).split(",")]
 
-    # Default-Länge für generierte Videos (in Sekunden)
     VIDEO_LENGTH = int(os.environ.get("VIDEO_LENGTH", "3600"))
