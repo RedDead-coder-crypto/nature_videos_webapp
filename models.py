9684d79399ee88ae9dcd38e2e21304c31f26e569
RedDead-coder-crypto <mppool@gmx.de>
2025-06-09 12:40:06 +0200

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db
 
 class Pipeline(db.Model):
+    __tablename__ = 'pipeline'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     status_text = db.Column(db.String(100), nullable=False, default="Warte auf Ausführung …")
